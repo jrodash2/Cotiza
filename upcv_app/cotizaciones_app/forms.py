781aed09aefee0926d71404e5efc99b20e3162fb
jrodash2 <61074808+jrodash2@users.noreply.github.com>
2026-01-17 16:49:11 -0600

    Unify cotizacion export layout

--- upcv_app/cotizaciones_app/forms.py
+++ upcv_app/cotizaciones_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from django.forms import BaseInlineFormSet, inlineformset_factory
 
 from .models import Cliente, ProductoServicio, Cotizacion, CotizacionItem
@@ -24,6 +25,9 @@ class ClienteForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if not self.instance.pk:
+            self.fields['fecha_emision'].initial = timezone.now().date()
+            self.fields['fecha_emision'].required = False
         for field in self.fields.values():
             if isinstance(field.widget, forms.CheckboxInput):
                 field.widget.attrs['class'] = 'form-check-input'
@@ -51,6 +55,9 @@ class ProductoServicioForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if not self.instance.pk:
+            self.fields['fecha_emision'].initial = timezone.now().date()
+            self.fields['fecha_emision'].required = False
         for field in self.fields.values():
             if isinstance(field.widget, forms.CheckboxInput):
                 field.widget.attrs['class'] = 'form-check-input'
@@ -68,7 +75,7 @@ class ProductoServicioForm(forms.ModelForm):
     def clean_precio_venta(self):
         precio_venta = self.cleaned_data.get('precio_venta')
         if precio_venta is not None and precio_venta < 0:
-            raise forms.ValidationError('El precio de venta no puede ser negativo.')
+            raise forms.ValidationError('El precio no puede ser negativo.')
         return precio_venta
 
 
@@ -92,6 +99,9 @@ class CotizacionForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if not self.instance.pk:
+            self.fields['fecha_emision'].initial = timezone.now().date()
+            self.fields['fecha_emision'].required = False
         for field in self.fields.values():
             if isinstance(field.widget, forms.CheckboxInput):
                 field.widget.attrs['class'] = 'form-check-input'
